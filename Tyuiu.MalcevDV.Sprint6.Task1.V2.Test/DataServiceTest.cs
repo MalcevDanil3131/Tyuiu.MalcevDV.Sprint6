7e0f52a5ec234a53ea36e79738d8976b14ef3309
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-12-27 04:06:11 +0500

    Task2

---
+++ Tyuiu.MalcevDV.Sprint6.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MalcevDV.Sprint6.Task1.V2.Lib;
+namespace Tyuiu.MalcevDV.Sprint6.Task1.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double[] result = ds.GetMassFunction(startValue, stopValue);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(11, result.Length);
+        }
+    }
+}
